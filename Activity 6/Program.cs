1faa00df5d3735e4d581a124f6fb4a1741164d3f
Admin <Admin@ELLA>
2025-08-09 06:00:02 +0800

    Add project files.

---
+++ Activity 6/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+List<string> cities = new List<string>();
+string city;
+
+Console.WriteLine("Enter city names (type 'exit' to finish):");
+
+// Input city names
+while (true)
+{
+    city = Console.ReadLine();
+    if (city.ToLower() == "exit") break;
+    cities.Add(city);
+}
+
+// Remove duplicates and sort cities
+var uniqueCities = cities.Distinct().OrderBy(c => c).ToList();
+
+Console.WriteLine("\nCities in alphabetical order:");
+foreach (var c in uniqueCities)
+{
+    Console.WriteLine(c);
+}
